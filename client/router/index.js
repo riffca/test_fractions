860d2e95dce8f7dad1c685d9d9f5e67ae1bada74
riffca <riffca@yandex.ru>
2018-04-16 06:05:11 +0300

    fix

--- client/router/index.js
+++ client/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../views/Home'
 
 
 const Calc =  () => import('../views/Calc')
@@ -11,10 +10,6 @@ Vue.use(Router)
 export default new Router({
   mode: 'hash',
   routes: [
-    {
-      path: '/',
-      component: Home
-    },
     {
       path: '/calc',
       component: Calc
